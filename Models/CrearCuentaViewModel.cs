6429e7674eca3360ad18083d2a643250b22bfbf4
Lili Lazaro <lilisapallanay@gmail.com>
2019-11-04 19:20:00 -0500

    index carousel

---
+++ Models/CrearCuentaViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+namespace ProyectoFinal_PM.Models
+{
+    public class CrearCuentaViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name="Correo electrónico")]
+        public string Correo { get; set; }
+
+        [Required]
+        [Display(Name="Contraseña")]
+        [DataType(DataType.Password)]
+        public string Password1 { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Confirmar Contraseña")]
+        [Compare("Password1", ErrorMessage = "Las contraseñas no coinciden")]
+        public string Password2 { get; set; }
+    }
+}
